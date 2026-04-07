4e0b9169c29f84b0d1e88a5d79dcc106e2b804a4
ParkJunYong <dyden119@gmail.com>
2026-04-07 23:31:02 +0900

    chore: api호출 전반적으로 잘 되는지 테스트

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,10 +11,87 @@ export default function Home() {
     }
   };
 
+  const handleCreateCategoryTest = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/categories",
+        {
+          name: "게임",
+        },
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("카테고리 생성 실패", error);
+    }
+  };
+
+  const handleGetCategoryTest = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/categories");
+      console.log(response.data);
+    } catch (error) {
+      console.error("카테고리 조회 실패", error);
+    }
+  };
+
+  const handleCreateBookmarkTest = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/api/bookmarks", {
+        categoryId: 1,
+        url: "https://www.google.com",
+        name: "Google",
+        description: "Google is a search engine",
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("북마크 생성 실패", error);
+    }
+  };
+
+  const handleGetBookmarkTest = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/bookmarks?categoryId=1",
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("북마크 조회 실패", error);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center gap-4 h-screen">
       <h1>react+spring boot 프로젝트</h1>
-      <button onClick={handleHealthCheck}>헬스 체크</button>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={handleHealthCheck}
+      >
+        헬스 체크
+      </button>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={handleCreateCategoryTest}
+      >
+        카테고리 생성 테스트
+      </button>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={handleGetCategoryTest}
+      >
+        카테고리 조회 테스트
+      </button>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={handleCreateBookmarkTest}
+      >
+        북마크 생성 테스트
+      </button>
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={handleGetBookmarkTest}
+      >
+        북마크 조회 테스트
+      </button>
     </div>
   );
 }
